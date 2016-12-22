ed1f0ba3fdb9b4ac2c35bb0b643d7f70b477cb04
kayway <eeu444@bangor.ac.uk>
2016-12-22 16:57:44 +0000

    Add files via upload

---
+++ ChatApplication/src/ChatClient.java
@@ -0,0 +1,56 @@
+// Import statements
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ * Assignment: Chat Applications
+ * Date: 22nd December 2016
+ * @author Kieran Ryan: eeu444
+ * @author Dainel Jones: eeu---
+ */
+public class ChatClient 
+{
+      /**
+     * The server port
+     */
+    private Socket socket;
+    /**
+    * @throws IOException
+    * @throws UnknownHostException
+    * @param serverName The server address
+    * @param port  The server port
+    */
+    public ChatClient(String serverName, int port) throws IOException
+    {
+        System.out.println("Establishing connection. Please wait...");
+         try
+        {
+            socket = new Socket(serverName, port);
+            System.out.println("Client socket created: " + socket);
+            // Reads keyboard input and outputs it to server
+            Scanner in = new Scanner(System.in);
+            PrintWriter output = new PrintWriter(socket.getOutputStream());
+            String line = "";
+            // Will close connection when the ".bye" is inputted in the console
+            while(!line.equals(".bye")) { line = in.nextLine(); output.println(line); output.flush(); }
+        }
+        catch(UnknownHostException event) {}
+        catch(IOException event){}
+        finally{ System.out.println("Closing connection..."); socket.close() ;}   // Error here :(
+    }
+    /**
+     * 
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException
+    {
+        ChatClient client;
+        if(args.length != 2) { System.out.println("Usage: java ChatClient ChatServer port"); }
+        
+        else{ String serverName = args[0]; int port = Integer.parseInt(args[1]); client = new ChatClient(serverName, port); } //Error here :(
+    }
+}
--- ChatApplication/src/ChatServer.java
+++ ChatApplication/src/ChatServer.java
@@ -1,13 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Import statements
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 
 /**
- *
- * @author obliv
+ * Assignment: Chat Applications
+ * Date: 22nd December 2016
+ * @author Kieran Ryan: eeu444
+ * @author Dainel Jones: eeu---
  */
-public class ChatServer {
-    
+
+public class ChatServer 
+{
+    /**
+     * The server port and address
+     */
+    private Socket socket;
+    private ServerSocket server;
+    /**
+     * 
+     * @param port
+     * @throws IOException 
+     */
+    public ChatServer(int port) throws IOException
+    {
+        try
+        {
+            server = new ServerSocket(port);
+            System.out.println("Server started" + server);
+            System.out.println("Waiting for connection...");
+            socket = server.accept();
+            System.out.println("Socket created: " + socket);
+            System.out.println("Connected to client...");
+            //Takes input from client
+            Scanner in = new Scanner(socket.getInputStream());
+            //Until the client enters ".bye", server will continue running
+            boolean done = false;
+            while(!done)
+            {
+                String line = in.nextLine();
+                System.out.println(line);
+                done = line.equals(".bye");
+            }
+        }
+        catch(IOException event) { System.out.println(event); }
+        
+        finally{ socket.close(); System.out.println("Connection closed..."); }
+    }
+    /**
+     * 
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException
+    {
+        ChatServer client;
+        if (args.length != 1) { System.out.println("Usage: java ChatServer port"); }
+        
+        else{ int port = Integer.parseInt(args[0]);  client = new ChatServer(port); }
+    }
 }
+
